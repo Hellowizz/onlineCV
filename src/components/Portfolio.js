f051d3d9f55ac98f447bf05a2046ec58eb9b301a
Hellowizz <heloroussel@hotmail.fr>
2021-03-23 22:59:49 +0100

    Portfolio part is done

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,3 +1,7 @@
+/* IMAGES */
+import hellowizzImg from '../img/hellowizz.png';
+import movieKwizzImg from '../img/moviekwizz.png';
+
 export default function Portfolio () {
   return (
     <div className="portfolio-container">
@@ -5,10 +9,17 @@ export default function Portfolio () {
       <h1 className="title">Portfolio</h1>
 
       <div className="projects-container">
-        <div className="project">Hellowizz art links</div>
-        <div className="project">Hello movie kwizz</div>
+        <div>
+          <a href="http://hellowizz.fr/"><div className="project" style={{background : 'center/cover no-repeat url(.' + hellowizzImg + ')'}} /></a>
+          <div className="project-name">Hellowizz links</div>
+          <div className="project-details">Site fait pour mes liens d'artiste fait en React.js</div>
+        </div>
+        <div>
+          <a href="https://github.com/Hellowizz/Hello-Movie-Kwizz"><div className="project" style={{background : 'center/cover no-repeat url(.' + movieKwizzImg + ')'}} /></a>
+          <div className="project-name">Hello Movie Kwizz</div>
+          <div className="project-details">Jeu de devinettes sur des films fait en React.js</div>
+        </div>
       </div>
-      <div>Github</div>
 
     </div>
   );
